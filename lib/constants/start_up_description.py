32e214bd7c4d1712f8953bcb93728f1d223a8433
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2021-09-21 00:00:42 -0400

    add comments about game flow

--- lib/constants/start_up_description.py
+++ lib/constants/start_up_description.py
@@ -1,13 +1,13 @@
 print('''
-    War Theory
+    WAR THEORY
     ----------
     Build a team of elite warriors and hold the line against waves of unrelenting orcs 
     seeking to destory your people and their way of life. In this turn based, text adventure 
     you will choose 3 heros to lead the fight against the northern hordes. Unlock new powers
     and call in allies when things get desparate. Finally, kill the orc Chieftain and save 
-    your village once and for all. 
+    the kingdom once and for all. 
 
-    Customization
+    GAME SETTINGS
     ----------
     Choose from 2 difficulty levels - Easy or Hard.
 
@@ -15,14 +15,14 @@ print('''
 
     Choose from 3 ally support maneuvers.
 
-    Settings
+    SETUP
     ---------
     Python3 is required to run the game
     Game commands are NOT case sensitive
 
-    Credits
+    CREDITS
     ----------
     Created by twjsanderson (2021)
-    Github - https://github.com/twjsanderson/war_theory
+    Repo - https://github.com/twjsanderson/war_theory
     
 ''')
--- lib/io_module/input_output.py
+++ lib/io_module/input_output.py
@@ -4,17 +4,28 @@ class Input_Ouput:
     string_to_lowercase = Helpers.string_to_lowercase
 
     @classmethod
-    def input_to_output(cls, prompt = '', instructions = None):
+    def take_input(cls, prompt = ''):
         user_input = ''
+
         if len(prompt) > 0:
             user_input = input(f'{prompt} ')
         else:
             user_input = input('')
+
         lower_case_user_input = cls.string_to_lowercase(user_input)
 
+        return lower_case_user_input
+
     @classmethod
-    def process_user_input(cls, user_input, instructions):
-        if isinstance(instructions, str):
-            return instructions
+    def process_input(cls, user_input, game_state, output):
+        # take user input in response to question
+        # update game state with choice
+        # output next step in game
+        return 
+
+    @classmethod
+    def process_next_step():
+        # take in game steps and process next prompt to be fed into take_input
+    
 
 
